88a995a5918543cbf49aad5e0b53c61188e1e052
wagner-eptvcampinas <wagner.eptvcampinas@gmail.com>
2026-06-05 17:16:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,15 @@
-const CACHE_NAME = 'pgm-eptv-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-  './icone_wcalc.png'
-];
+const CACHE_NAME = 'pgm-eptv-v2';
 
-// Instala o Service Worker e guarda os arquivos no cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  self.skipWaiting();
 });
 
-// Ativa o Service Worker
 self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
 });
 
-// Serve os arquivos direto do cache quando estiver offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
-
